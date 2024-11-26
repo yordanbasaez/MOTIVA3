1f47de99580ba0da85af076cacdbd18b226c2c95
yordanbasaez <yordan.zohan@gmail.com>
2024-11-26 01:21:46 -0300

    Implementación Tabla inscripciones

--- app/src/main/java/Viewmodel/InscripcionViewModel.kt
+++ app/src/main/java/Viewmodel/InscripcionViewModel.kt
@@ -2,23 +2,28 @@ package com.tuapp.motiva3.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.tuapp.motiva3.database.InscripcionRepository
 import com.tuapp.motiva3.modelo.Inscripciones
-import kotlinx.coroutines.flow.Flow
+import com.tuapp.motiva33.database.InscripcionDao
 import kotlinx.coroutines.launch
 
-class InscripcionViewModel(private val inscripcionRepository: InscripcionRepository) : ViewModel() {
+class InscripcionesViewModel(private val inscripcionesDao: InscripcionDao) : ViewModel() {
 
-    // Método para inscribir un usuario en un taller
-    fun inscribirUsuarioEnTaller(tallerId: Int, usuarioId: String) {
+    fun agregarInscripcion(inscripcion: Inscripciones) {
         viewModelScope.launch {
-            val inscripcion = Inscripciones(idTaller = tallerId, idUsuario = usuarioId)
-            inscripcionRepository.insertarInscripcion(inscripcion)
+            inscripcionesDao.agregarInscripcion(inscripcion)
         }
     }
 
-    // Método para obtener las inscripciones de un taller (retorna un Flow de Inscripciones)
-    fun obtenerInscripcionesPorTaller(tallerId: Int): Flow<List<Inscripciones>> {
-        return inscripcionRepository.obtenerInscripcionesPorTaller(tallerId)
+    fun obtenerInscripcionesPorUsuario(idUsuario: String, onResult: (List<Inscripciones>) -> Unit) {
+        viewModelScope.launch {
+            val inscripciones = inscripcionesDao.obtenerInscripcionesPorUsuario(idUsuario)
+            onResult(inscripciones)
+        }
+    }
+
+    fun eliminarInscripcion(inscripcion: Inscripciones) {
+        viewModelScope.launch {
+            inscripcionesDao.eliminarInscripcion(inscripcion)
+        }
     }
 }
