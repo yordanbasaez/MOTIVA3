1566588cca0df957db430dae00704f9ebfa2cf79
yordanbasaez <yordan.zohan@gmail.com>
2024-11-26 01:21:46 -0300

    Implementación Tabla inscripciones

--- app/src/main/java/database/InscripcionDao.kt
+++ app/src/main/java/database/InscripcionDao.kt
@@ -1,16 +1,17 @@
 package com.tuapp.motiva33.database
 
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.Query
+import androidx.room.*
 import com.tuapp.motiva3.modelo.Inscripciones
-import kotlinx.coroutines.flow.Flow
 
 @Dao
-interface InscripcionDao {
-    @Query("SELECT * FROM Inscripciones WHERE idTaller = :tallerId")
-    fun getInscripcionesPorTaller(tallerId: Int): Flow<List<Inscripciones>>
+interface InscripcionesDao {
 
-    @Insert
-    suspend fun insertarInscripcion(inscripcion: Inscripciones)
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun agregarInscripcion(inscripcion: Inscripciones)
+
+    @Query("SELECT * FROM Inscripciones WHERE idUsuario = :idUsuario")
+    suspend fun obtenerInscripcionesPorUsuario(idUsuario: String): List<Inscripciones>
+
+    @Delete
+    suspend fun eliminarInscripcion(inscripcion: Inscripciones)
 }
